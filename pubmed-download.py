72647dc4ae8b74fe01f18163f616fbef8b59fd3f
Sebastian Fischer <fischer.sebastian@gmail.com>
2017-12-09 18:30:26 +0100

    Add ability to download BioC records from Pubmed
    Use pubmed-download.py with the command line option --bioc
    to retrieve BioC instead of standard XML.

--- pubmed-download.py
+++ pubmed-download.py
@@ -9,23 +9,53 @@ import time
 import requests
 from requests import Request, Session
 
+def document_for_pmid(pmid, format="xml"):
+    if format == "bioc":
+        url = "https://www.ncbi.nlm.nih.gov/bionlp/RESTful/pubmed.cgi/BioC_xml/{:d}/unicode"
+        url = url.format(pmid)
+        params = {}
+    else:
+        url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+        params = {"db"      : "pubmed",
+                  "retmode" : "xml",
+                  "id"      : int(pmid) }
+    req = Request("GET", url, params=params)
+    req = req.prepare()
+    try:
+        response = session.send(req)
+    except requests.exceptions.ConnectionError:
+        # handle API rate limiting
+        print("HTTP connection failed. Retrying in 3 seconds...")
+        time.sleep(3)
+        response = session.send(req)
+
+    response.raise_for_status()
+
+    return response.text
+
 # parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("input_file", help="a file containing one or more "
     + "pmids, separated by newlines")
 parser.add_argument("output_dir", help="the directory in which to store "
     + "the downloaded XML file(s)")
+parser.add_argument("-b", "--bioc", help="download BioC XML",
+        action="store_true")
 parser.add_argument("-c", "--continue", dest="cont",
         help="don't quit when an operation fails", action="store_true")
 parser.add_argument("-o", "--overwrite",
         help="overwrite existing files", action="store_true")
 args = parser.parse_args()
 
-url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
-params = {"db"      : "pubmed",
-          "retmode" : "xml" }
+# requests setup
 session = Session()
-req = Request("GET", url, params=params)
+
+if args.bioc:
+    format = "bioc"
+    ext = ".bioc.xml"
+else:
+    format = "xml"
+    ext = ".xml"
 
 # read the pmids to be downloaded
 with open(args.input_file) as f:
@@ -36,30 +66,17 @@ print("Will download {} documents.".format(len(pmids)))
 pmids = [int(pmid) for pmid in pmids]
 
 for pmid in pmids:
-    file_path = Path(args.output_dir, str(pmid) + ".xml")
+    file_path = Path(args.output_dir, str(pmid) + ext)
     if file_path.exists() and not args.overwrite:
         print("{} already exists. Skipping.".format(file_path))
         continue
 
     print("Downloading pubmed document #{}... ".format(pmid), end='')
-    req.params["id"] = pmid
-    prepped = req.prepare()
-    try:
-        response = session.send(prepped)
-    except requests.exceptions.ConnectionError:
-        # handle API rate limiting
-        print("HTTP connection failed. Retrying in 3 seconds...")
-        time.sleep(3)
-        response = session.send(prepped)
 
-    if response.status_code != 200:
-        if args.cont:
-            continue
-        else:
-            response.raise_for_status()
+    text = document_for_pmid(pmid, format)
 
     with file_path.open("w") as f:
-        f.write(response.text)
+        f.write(text)
     print("successful.")
 
     # pubmed doesn't like too many requests at once, so we wait a bit
